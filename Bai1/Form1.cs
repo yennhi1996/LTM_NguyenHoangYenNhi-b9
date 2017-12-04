be7b2b853d44e7e185e475934984a8e132304694
nataurus9x <nataurus9x@gmail.com>
2017-12-04 10:38:04 +0700

    Buoi9

---
+++ Bai1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+using System.Net;
+namespace Bai1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            SmtpClient mailclient = new SmtpClient("smtp.gmail.com", 587);
+            mailclient.EnableSsl = true;
+            mailclient.Credentials = new NetworkCredential("abc@gmail.com", "password");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            SmtpClient mailclient = new SmtpClient("smtp.gmail.com", 587);
+            mailclient.EnableSsl = true;
+            mailclient.Credentials = new NetworkCredential("abc@gmail.com", "password");
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            MailMessage message = new MailMessage("abc@gmail.com", "xyz@gmail.com");
+            message.Subject = "";
+        }
+    }
+}
---
+++ Mail/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace Mail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SmtpClient mailclient = new SmtpClient("smtp.gmail.com", 587);
+            mailclient.EnableSsl = true;
+            mailclient.Credentials = new NetworkCredential("abc@gmail.com","password");
+            MailMessage message = new MailMessage("abc@gmail.com", "xyz@gmail.com");
+            message.Subject = "Hello";
+            message.Body = "I'm GoKu";
+            mailclient.Send(message);
+            Console.WriteLine("Mail sent successfully");
+        }
+    }
+}
